e219948566798564fcbbcd83482bf8d9b3a779bf
dhikydher77 <123530158+dhikydher77@users.noreply.github.com>
2024-08-21 18:52:25 +0700

    footer

--- app/(website)/layout.js
+++ app/(website)/layout.js
@@ -1,11 +1,149 @@
 import DrawerNav from "../component/navbar";
+import Image from "next/image";
+import Link from "next/link";
+import {
+  RiFacebookBoxFill,
+  RiInstagramFill,
+  RiTwitterFill,
+  RiYoutubeFill,
+} from "react-icons/ri";
+import Footer from "../component/footer";
 export const revalidate = 60;
 export default function WebsiteLayout({ children }) {
   return (
     <section>
       <DrawerNav />
       {children}
-      <div>ini footer</div>
+      <Footer/>
+      {/* <footer className="bg-core py-6 px-8 text-center">
+       
+        <div className="hidden md:grid md:grid-cols-4 md:gap-8 md:text-left md:items-start md:px-8 pb-8 pt-2 border-b-[1px] border-white">
+          <div className="flex justify-center md:justify-start mb-4 md:mb-0">
+            <Image
+              src="/logo.png"
+              title="logo hasanahub"
+              className="h-[80px] w-[90px] mx-auto md:mx-0"
+              width={100}
+              height={100}
+              alt="logo hasanahub"
+            />
+          </div>
+          <div className="flex items-center justify-center md:justify-start">
+            <div className="flex flex-col md:space-y-2">
+              <Link href="/" className="block mb-2 font-medium text-slate-50 hover:underline">
+                Beranda
+              </Link>
+              <Link href="/umrah" className="block mb-2 font-medium text-slate-50 hover:underline">
+                Umrah
+              </Link>
+              <Link href="/gallery" className="block mb-2 font-medium text-slate-50 hover:underline">
+                Galeri
+              </Link>
+              <Link href="/artikel" className="block mb-2 font-medium text-slate-50 hover:underline">
+                Artikel
+              </Link>
+              <Link href="/aboutus" className="block mb-2 font-medium text-slate-50 hover:underline">
+                Tentang Kami
+              </Link>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex md:hidden justify-between items-center px-8">
+          <Image
+            src="/logohub.png"
+            title="logo hasanahub"
+            className="h-[60px] w-[60px]"
+            width={100}
+            height={100}
+            alt="logo hasanahub"
+          />
+          <div className="text-sm">© Copyright HasanahHub 2024</div>
+          <div className="flex items-center text-3xl gap-2">
+            <Link href="/">
+              <RiInstagramFill />
+            </Link>
+            <Link href="/">
+              <RiFacebookBoxFill />
+            </Link>
+            <Link href="/">
+              <RiYoutubeFill />
+            </Link>
+            <Link href="/">
+              <RiTwitterFill />
+            </Link>
+          </div>
+        </div>
+
+   
+        <div className="hidden md:flex md:justify-between md:items-center md:mt-4 md:px-8">
+          <div className="flex items-center text-3xl gap-2 text-slate-300">
+            <Link href="/">
+              <RiInstagramFill />
+            </Link>
+            <Link href="/">
+              <RiFacebookBoxFill />
+            </Link>
+            <Link href="/">
+              <RiYoutubeFill />
+            </Link>
+            <Link href="/">
+              <RiTwitterFill />
+            </Link>
+          </div>
+
+          <div className="text-sm text-[#bfc4c7]">
+            © Copyright HasanahHub 2024
+          </div>
+
+          <div className="flex items-center">
+            <Switch size="md" colorScheme="teal" />
+            <span className="ml-2">EN / ID</span>
+          </div>
+        </div>
+
+
+      
+        <div className="md:hidden">
+          <div className="text-center mx-auto mb-4">
+            <Image
+              src="/logohub.png"
+              title="logo hasanahub"
+              className="h-[60px] w-[60px] mx-auto"
+              width={100}
+              height={100}
+              alt="logo hasanahub"
+            />
+          </div>
+          <div className="flex items-center font-bold justify-center gap-x-4 gap-y-2 py-2 underline flex-wrap">
+            <Link href="/">Beranda</Link>
+            <Link href="/umrah">Umrah</Link>
+            <Link href="/gallery">Galeri</Link>
+            <Link href="/artikel">Artikel</Link>
+            <Link href="/aboutus">Tentang Kami</Link>
+          </div>
+          <p className="my-4">
+            Jl. Jendral Sudirman No 60 Sempur Kec Bogor Tengah Kota Bogor
+          </p>
+          <div className="font-bold">62-812-345-6789 | 0251-75923xx</div>
+          <div className="font-bold">info@hanasahhub.com</div>
+          <div className="flex items-center text-3xl gap-2 mt-4 text-semicore mb-4 justify-center">
+            <Link href="/">
+              <RiInstagramFill />
+            </Link>
+            <Link href="/">
+              <RiFacebookBoxFill />
+            </Link>
+            <Link href="/">
+              <RiYoutubeFill />
+            </Link>
+            <Link href="/">
+              <RiTwitterFill />
+            </Link>
+          </div>
+          <div className="text-sm">© Copyright HasanahHub 2024</div>
+        </div>
+      </footer> */}
     </section>
   );
 }
---
+++ app/component/footer.js
@@ -0,0 +1,159 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import {
+  RiFacebookBoxFill,
+  RiInstagramFill,
+  RiTwitterFill,
+  RiYoutubeFill,
+} from "react-icons/ri";
+import { useState } from "react";
+
+export default function Footer() {
+  const [isEnglish, setIsEnglish] = useState(false);
+
+  const handleLanguageSwitch = (language) => {
+    setIsEnglish(language === "en");
+  };
+
+  return (
+    <footer className="bg-core py-6 px-8 text-center relative">
+      {/* Desktop Footer */}
+      <div className="hidden md:flex md:flex-row md:items-center md:gap-8 md:py-5 md:px-16">
+        {/* Logo Column */}
+        <div className="w-[15%] flex justify-center md:justify-start mb-4 md:mb-0">
+          <Image
+            src="/logo.png"
+            title="logo hasanahub"
+            className="h-[110px] w-[110px] mx-auto md:mx-0"
+            width={100}
+            height={100}
+            alt="logo hasanahub"
+          />
+        </div>
+
+        {/* Main Content Column */}
+        <div className="w-[85%] flex md:flex-col">
+          {/* Upper Row */}
+          <div className="flex justify-between w-full mb-4">
+            <div className="flex flex-row gap-8 font-montserrat text-base">
+              <Link
+                href="/"
+                className="mb-2 font-medium text-black hover:underline"
+              >
+                Beranda
+              </Link>
+              <Link
+                href="/tur"
+                className="mb-2 font-medium text-black hover:underline"
+              >
+                Tur
+              </Link>
+              <Link
+                href="/galeri"
+                className="mb-2 font-medium text-black hover:underline"
+              >
+                Galeri
+              </Link>
+              <Link
+                href="/artikel"
+                className="mb-2 font-medium text-black hover:underline"
+              >
+                Artikel
+              </Link>
+              <Link
+                href="/aboutus"
+                className="mb-2 font-medium text-black hover:underline"
+              >
+                Tentang Kami
+              </Link>
+            </div>
+            <div className="flex items-center text-3xl gap-2 text-black">
+              <Link href="/">
+                <RiInstagramFill />
+              </Link>
+              <Link href="/">
+                <RiFacebookBoxFill />
+              </Link>
+              <Link href="/">
+                <RiYoutubeFill />
+              </Link>
+              <Link href="/">
+                <RiTwitterFill />
+              </Link>
+            </div>
+          </div>
+
+          <div className="border-t-[1px] border-black w-full my-4"></div>
+
+          {/* Lower Row */}
+          <div className="flex justify-between w-full">
+            <div className="text-sm text-[#333] flex-1 flex justify-center items-end">
+            © 2024 - 3Q Cloud Trip - PT. Mumbul Kreasi Abadi
+            </div>
+            <div className="flex items-center">
+              <div className="language-switch bg-white rounded-lg p-1 flex">
+                <button
+                  className={`px-4 py-2 ${!isEnglish ? "bg-blue-500 text-white" : "text-blue-500"
+                    }`}
+                  onClick={() => handleLanguageSwitch("id")}
+                >
+                  Indonesia
+                </button>
+                <button
+                  className={`px-4 py-2 ${isEnglish ? "bg-blue-500 text-white" : "text-blue-500"
+                    }`}
+                  onClick={() => handleLanguageSwitch("en")}
+                >
+                  English
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Mobile Footer */}
+      <div className="md:hidden">
+        <div className="text-center mx-auto mb-4">
+          <Image
+            src="/logohub.png"
+            title="logo hasanahub"
+            className="h-[60px] w-[60px] mx-auto"
+            width={100}
+            height={100}
+            alt="logo hasanahub"
+          />
+        </div>
+        <div className="flex items-center font-bold justify-center gap-x-4 gap-y-2 py-2 underline flex-wrap">
+          <Link href="/">Beranda</Link>
+          <Link href="/umrah">Umrah</Link>
+          <Link href="/gallery">Galeri</Link>
+          <Link href="/artikel">Artikel</Link>
+          <Link href="/aboutus">Tentang Kami</Link>
+        </div>
+        <p className="my-4">
+          Jl. Jendral Sudirman No 60 Sempur Kec Bogor Tengah Kota Bogor
+        </p>
+        <div className="font-bold">62-812-345-6789 | 0251-75923xx</div>
+        <div className="font-bold">info@hanasahhub.com</div>
+        <div className="flex items-center text-3xl gap-2 mt-4 text-semicore mb-4 justify-center">
+          <Link href="/">
+            <RiInstagramFill />
+          </Link>
+          <Link href="/">
+            <RiFacebookBoxFill />
+          </Link>
+          <Link href="/">
+            <RiYoutubeFill />
+          </Link>
+          <Link href="/">
+            <RiTwitterFill />
+          </Link>
+        </div>
+        <div className="text-sm">© Copyright HasanahHub 2024</div>
+      </div>
+    </footer>
+  );
+}
